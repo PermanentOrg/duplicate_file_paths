a5a6259ee50bc7151232867c7055b6fd3e39df09
Cristina <hi@xmunoz.com>
2021-11-04 21:07:09 +0000

    Eliminate vault links from query

--- find_duplicate_files/models.py
+++ find_duplicate_files/models.py
@@ -20,6 +20,7 @@ class Archive:
         Folder types excluded because deprecated:
          - type.folder.vault
          - type.folder.root.vault
+         - type.folder_link.vault
 
         Folder types excluded because they belong to a different archive:
          - type.folder.root.share
@@ -27,14 +28,15 @@ class Archive:
          - type.folder_link.share
         """
 
-        logging.debug("Finding dupe paths for archive %d", self.archive_id)
+        # logging.debug("Finding dupe paths for archive %d", self.archive_id)
         query = (
             "SELECT folder.folderId, folder.displayName, folder.type, "
             "folder_link.parentFolderId FROM folder "
             "INNER JOIN folder_link ON folder.folderId = folder_link.folderId "
             "WHERE folder.archiveId = %s AND folder.type NOT IN "
             "('type.folder.vault', 'type.folder.root.vault', 'type.folder.root.share') "
-            "AND folder_link.type NOT IN ('type.folder_link.root.share', 'type.folder_link.share')"
+            "AND folder_link.type NOT IN "
+            "('type.folder_link.root.share', 'type.folder_link.share', 'type.folder_link.vault')"
         )
 
         self.cur.execute(query, (self.archive_id,))
@@ -46,25 +48,27 @@ class Archive:
                 "parent_id": parent_folder_id,
             }
 
-        folders_with_paths = recursively_organize_folder_paths(folders)
+        folders_with_paths = self.recursively_organize_folder_paths(folders)
 
         return False
 
-
-def recursively_organize_folder_paths(folders):
-    folders_with_paths = {}
-    for folder_id, values in folders.items():
-        parent_id = values["parent_id"]
-        path = "/" + values["name"]
-        if parent_id:
-            while parent_id:
-                if parent_id not in folders:
-                    logging.debug('Error, missing parent_id in archive: %d', parent_id)
-                    parent_id = None
-                else:
-                    parent = folders[parent_id]
-                    path = "/" + parent["name"] + path
-                    parent_id = parent["parent_id"]
-        #logging.debug(path)
-        folders_with_paths[folder_id] = {"type": values["type"], "path": path}
-    return folders_with_paths
+    def recursively_organize_folder_paths(self, folders):
+        folders_with_paths = {}
+        for folder_id, values in folders.items():
+            parent_id = values["parent_id"]
+            path = "/" + values["name"]
+            if parent_id:
+                while parent_id:
+                    if parent_id not in folders:
+                        logging.debug(
+                            "Error, missing parent_id %d in archive: %d",
+                            parent_id,
+                            self.archive_id,
+                        )
+                        parent_id = None
+                    else:
+                        parent = folders[parent_id]
+                        path = "/" + parent["name"] + path
+                        parent_id = parent["parent_id"]
+            folders_with_paths[folder_id] = {"type": values["type"], "path": path}
+        return folders_with_paths
--- find_duplicate_files/permanent.py
+++ find_duplicate_files/permanent.py
@@ -1,5 +1,7 @@
 def get_archives(cur):
-    query = "SELECT archiveId FROM archive WHERE type IS NOT NULL ORDER BY archiveId DESC LIMIT 1000"
+    query = (
+        "SELECT archiveId FROM archive WHERE type IS NOT NULL ORDER BY archiveId DESC"
+    )
     cur.execute(query)
     all_archive_ids = [archiveId[0] for archiveId in cur]
     return all_archive_ids
