e63cb405a7ec0f0982eecf9336d60cf90ba3277a
Cristina Muñoz <hi@xmunoz.com>
2021-11-08 16:18:51 -0500

    Further refine archive query

--- find_duplicate_files/permanent.py
+++ find_duplicate_files/permanent.py
@@ -4,6 +4,8 @@ def get_archives(cur):
         "INNER JOIN account_archive ON archive.archiveId = account_archive.archiveId "
         "INNER JOIN account ON account_archive.accountId = account.accountId "
         "WHERE archive.type IS NOT NULL AND archive.status LIKE 'status.generic.ok' "
+        "AND account_archive.status like 'status.generic.ok' "
+        "AND account_archive.accessRole like 'access.role.owner'"
         "AND account.type LIKE 'type.account.standard' ORDER BY archive.archiveId DESC"
     )
     cur.execute(query)
